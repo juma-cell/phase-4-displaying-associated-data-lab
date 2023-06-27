dbc997f362c1f7412f32c05f92c8156ddba67e06
Juma <juma.musa@student.moringaschool.com>
2023-06-27 13:39:53 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all
+      render json: items_with_users(items)
+    end
+  
+    private
+  
+    def items_with_users(items)
+      items.map do |item|
+        {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          price: item.price,
+          user: {
+            id: item.user.id,
+            username: item.user.username,
+            city: item.user.city
+          }
+        }
+      end
+    end
+  end
+  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
-end
+    def show
+      user = User.find(params[:id])
+      render json: user_with_items(user)
+    end
+  
+    private
+  
+    def user_with_items(user)
+      {
+        id: user.id,
+        username: user.username,
+        city: user.city,
+        items: user.items.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            price: item.price
+          }
+        end
+      }
+    end
+  end
+  
